2a125baad2740f8e23e5e165aad9553d9828e6e4
HopeMashal <mashal.hope@gmail.com>
2022-08-29 17:23:42 +0300

    edit app testng test file

--- task_summary/src/test/java/task/AppTestNGTest.java
+++ task_summary/src/test/java/task/AppTestNGTest.java
@@ -34,80 +34,80 @@ public class AppTestNGTest {
 
   @Test(dataProvider = "input")
   public void getCircleArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int radiusA = Integer.parseInt(RadiusA);
+    double radiusA =  Double.parseDouble(RadiusA);
     Circle circle1 = new Circle(radiusA);
     double ExArea=circle1.getArea();
     double AcArea=Math.PI*Math.pow(radiusA, 2);
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getEllipseArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int radiusA = Integer.parseInt(RadiusA);
-    int radiusB = Integer.parseInt(RadiusB);
+    double radiusA =  Double.parseDouble(RadiusA);
+    double radiusB =  Double.parseDouble(RadiusB);
     Ellipse ellipse1 = new Ellipse(radiusA, radiusB);
     double ExArea=ellipse1.getArea();
     double AcArea=radiusA*radiusB*Math.PI;
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getParallelogramArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int baseA = Integer.parseInt(BaseA);
-    int height = Integer.parseInt(Height);
+    double baseA =  Double.parseDouble(BaseA);
+    double height =  Double.parseDouble(Height);
     Parallelogram parallelogram1 = new Parallelogram(baseA, height);
     double ExArea=parallelogram1.getArea();
     double AcArea=baseA*height;
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getRectangleArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int baseA = Integer.parseInt(BaseA);
-    int height = Integer.parseInt(Height);
+    double baseA =  Double.parseDouble(BaseA);
+    double height =  Double.parseDouble(Height);
     Rectangle rectangle1 = new Rectangle(baseA, height);
     double ExArea=rectangle1.getArea();
     double AcArea=baseA*height;
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getSectorArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int radiusA = Integer.parseInt(RadiusA);
-    int angleDeg = Integer.parseInt(AngleDeg);
+    double radiusA =  Double.parseDouble(RadiusA);
+    double angleDeg =  Double.parseDouble(AngleDeg);
     Sector sector1 = new Sector(angleDeg, radiusA);
     double ExArea=sector1.getArea();
     double AcArea=0.5*(((Math.PI)/180)*angleDeg)*Math.pow(radiusA, 2);
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getSquareArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int height = Integer.parseInt(Height);
+    double height =  Double.parseDouble(Height);
     Square square1 = new Square(height);
     double ExArea=square1.getArea();
     double AcArea=Math.pow(height, 2);
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getTrapezoidArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int baseA = Integer.parseInt(BaseA);
-    int baseB = Integer.parseInt(BaseB);
-    int height = Integer.parseInt(Height);
+    double baseA =  Double.parseDouble(BaseA);
+    double baseB =  Double.parseDouble(BaseB);
+    double height =  Double.parseDouble(Height);
     Trapezoid trapezoid1 = new Trapezoid(baseA, baseB, height);
     double ExArea=trapezoid1.getArea();
     double AcArea=(0.5*(baseA+baseB))*height;
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 
   @Test(dataProvider = "input")
   public void getTriangleArea(String AngleDeg,String BaseA,String BaseB,String Height,String RadiusA,String RadiusB){
-    int baseA = Integer.parseInt(BaseA);
-    int height = Integer.parseInt(Height);
+    double baseA =  Double.parseDouble(BaseA);
+    double height =  Double.parseDouble(Height);
     Triangle triangle1 = new Triangle(baseA, height);
     double ExArea=triangle1.getArea();
     double AcArea=0.5*baseA*height;
-    Assert.assertEquals(ExArea, AcArea);
+    Assert.assertEquals(AcArea, ExArea, "LENGTHS MUST BE POSITIVE NUMBERS");
   }
 }
